fba671f33225ab70676bebaf70f5da4cbcdd0fa9
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-19 18:50:22 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -10,8 +10,5 @@ unsigned char itc_toUpper(unsigned char c);
 unsigned char itc_changeCase(unsigned char c);
 long long itc_len_middle(string str);
 bool itc_compare(string s1, string s2);
-int itc_countWords(string str);
-bool itc_symbol_middle(string str);
-string itc_slice_middle(string str, int s_start, int s_end);
-int itc_mymin_middle(int a, int b);
+
 #endif // MIDDLE_STR_H_INCLUDED
