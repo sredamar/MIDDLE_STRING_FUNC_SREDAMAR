36fccd5d8f6799b6e85e3f9587d4a5be8a3a8a16
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-19 19:08:03 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -10,5 +10,5 @@ unsigned char itc_toUpper(unsigned char c);
 unsigned char itc_changeCase(unsigned char c);
 long long itc_len_middle(string str);
 bool itc_compare(string s1, string s2);
-
+int itc_countWords(string str);
 #endif // MIDDLE_STR_H_INCLUDED
