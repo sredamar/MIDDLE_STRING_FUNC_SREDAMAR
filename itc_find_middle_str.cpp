5bda087a7f01d5283b14a5ab19e0e0ae85140b0c
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-20 12:07:14 +0300

    Add files via upload

---
+++ itc_find_middle_str.cpp
@@ -0,0 +1,14 @@
+#include "middle_str.h"
+
+int itc_find_middle_str(string str1, string str2){
+    long long len1 = itc_len_middle(str1), len2 = itc_len_middle(str2), counter, i1 = 0, i2 = 0;
+    while (i1 < len1){
+        counter = 0;
+        if (str1[i1] == str2[0]){
+        for (long long i2 = 0; i2 < len2; i2++){
+            if (str1[i1 + i2] == str2[i2])
+                counter += 1;}}
+        if (counter == len2)
+            return i1;
+    i1++;}
+    return -1;}
---
+++ itc_isFirstInSecond.cpp
@@ -0,0 +1,7 @@
+#include "middle_str.h"
+
+bool itc_isFirstInSecond(string s1, string s2) {
+    long long checker = itc_find_middle_str(s1, s2);
+    if (checker == -1)
+        return false;
+return true;}
