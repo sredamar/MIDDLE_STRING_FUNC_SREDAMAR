548768e59f3e3aeb6ac29b33a7f3b7cf6a0bbd6d
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-20 13:57:38 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -4,14 +4,17 @@
 #include <string>
 using namespace std;
 
-bool itc_isDigit(unsigned char c);
-unsigned char itc_toUpper(unsigned char c);
-unsigned char itc_changeCase(unsigned char c);
-long long itc_len_middle(string str);
-bool itc_compare(string s1, string s2);
-int itc_countWords(string str);
-char itc_sameChar(string str);
+bool itc_isDigit(unsigned char c); //1
+unsigned char itc_toUpper(unsigned char c); //2
+unsigned char itc_changeCase(unsigned char c); //3
+long long itc_len_middle(string str); //dlina_stroki
+bool itc_compare(string s1, string s2); //4
+int itc_countWords(string str); //5
+char itc_sameChar(string str); //7
 bool itc_isFirstInSecond(string s1, string s2); //8
 int itc_find_middle_str(string str1, string str2); //podstroka
-string itc_Cezar(string str, int k);
+string itc_Cezar(string str, int k); //9
+string itc_slice_middle_str(string str, int s_start, int s_end); //help_func_10
+int itc_mymin_middle_str(int a, int b); //minimum_chisel_dlya_10func
+string itc_rmFreeSpace(string str); //10
 #endif // MIDDLE_STR_H_INCLUDED
