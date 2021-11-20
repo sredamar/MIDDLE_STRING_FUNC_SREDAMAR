8f08d22e69e18ebb72e066f334d47b2a563843bb
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-20 13:02:10 +0300

    func 3

--- itc_changeCase.cpp
+++ itc_changeCase.cpp
@@ -1,6 +1,5 @@
- #include "middle_str.h"
-
- unsigned char itc_changeCase(unsigned char c) {
+#include "middle_str.h"
+unsigned char itc_changeCase(unsigned char c) {
     unsigned char n_reg = c + 32;
     if ((c >= 'a') && (c <= 'z')) {
         return itc_toUpper(c);}
