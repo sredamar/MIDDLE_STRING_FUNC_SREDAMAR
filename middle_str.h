3664a3067a6d8d0a18772d3597e608e49916ac01
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-20 12:07:46 +0300

    Update middle_str.h

--- middle_str.h
+++ middle_str.h
@@ -12,4 +12,6 @@ long long itc_len_middle(string str);
 bool itc_compare(string s1, string s2);
 int itc_countWords(string str);
 char itc_sameChar(string str);
+bool itc_isFirstInSecond(string s1, string s2); //8
+int itc_find_middle_str(string str1, string str2); //podstroka
 #endif // MIDDLE_STR_H_INCLUDED
