f2e44ddeea9a29fb3d66e0e3e9a46a29c03b2102
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-20 13:04:12 +0300

    func help_8

--- itc_find_middle_str.cpp
+++ itc_find_middle_str.cpp
@@ -1,5 +1,4 @@
 #include "middle_str.h"
-
 int itc_find_middle_str(string str1, string str2){
     long long len1 = itc_len_middle(str1), len2 = itc_len_middle(str2), counter, i1 = 0, i2 = 0;
     while (i1 < len1){
