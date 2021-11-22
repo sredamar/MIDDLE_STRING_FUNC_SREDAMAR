3f93aada45b43442d5c5d5841ad8fef7e493c9c1
sredamar <92477629+sredamar@users.noreply.github.com>
2021-11-22 22:31:45 +0300

    Update itc_find_middle_str.cpp

--- itc_find_middle_str.cpp
+++ itc_find_middle_str.cpp
@@ -1,13 +1,15 @@
 #include "middle_str.h"
-int itc_find_middle_str(string str1, string str2){
-    long long len1 = itc_len_middle(str1), len2 = itc_len_middle(str2), counter, i1 = 0, i2 = 0;
-    while (i1 < len1){
+
+int itc_find_str(string str1, string str2){
+    long long len = itc_len(str1);
+    long long length = itc_len(str2);
+    long long counter = 0;
+    for (long long i = 0; i < len; i++){
         counter = 0;
-        if (str1[i1] == str2[0]){
-        for (long long i2 = 0; i2 < len2; i2++){
-            if (str1[i1 + i2] == str2[i2])
-                counter += 1;}}
-        if (counter == len2)
-            return i1;
-    i1++;}
-    return -1;}
+        if (str1[i] == str2[0]){
+        for (long long i1 = 0; i1 < length; i1++){
+            if (str1[i + i1] == str2[i1]){
+                counter += 1;}}}
+        if (counter == length){
+            return i;}}
+    return (-1);}
