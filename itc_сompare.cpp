4712f1704cffe94fa7a8d071d048b90b9dc39b5e
sredamar <92477629+sredamar@users.noreply.github.com>
2021-12-02 19:15:17 +0300

    Update itc_сompare.cpp

--- itc_сompare.cpp
+++ itc_сompare.cpp
@@ -1,6 +1,6 @@
 #include "middle_str.h"
 
-bool itc_compare(string s1, string s2) {
+bool itc_Сompare(string s1, string s2) {
     long long len1 = itc_len_middle(s1), len2 = itc_len_middle(s2), i = 0, counter = 0;
         if (len1 == len2) {
         while (s1[i] != '\0') {
